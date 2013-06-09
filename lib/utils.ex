efd84b679ac68dfbeca5a929ee553baf930a857c
RJ Nowling <rnowling@nd.edu>
2013-06-08 20:33:06 -0400

    Converted everything to spaces

--- lib/utils.ex
+++ lib/utils.ex
@@ -1,83 +1,83 @@
 defmodule GeneticAlgorithms.Utils do
-	def sum(coll) do
-		List.foldl(coll, 0, &1 + &2)
-	end
+  def sum(coll) do
+    List.foldl(coll, 0, &1 + &2)
+  end
 
-	defp recur_apply(input, func, 1) do
-		func.(input)
-	end
+  defp recur_apply(input, func, 1) do
+    func.(input)
+  end
 
-	defp recur_apply(input, func, times) do
-		output = func.(input)
-		recur_apply(output, func, times - 1)
-	end
+  defp recur_apply(input, func, times) do
+    output = func.(input)
+    recur_apply(output, func, times - 1)
+  end
 
-	defp foldr(array, function) do
-		last_idx = :array.size(array) - 1
-		elem = :array.get(last_idx, array)
-		_foldr(array, function, elem, last_idx - 1)
-	end
+  defp foldr(array, function) do
+    last_idx = :array.size(array) - 1
+    elem = :array.get(last_idx, array)
+    _foldr(array, function, elem, last_idx - 1)
+  end
 
-	defp _foldr(array, function, result, 0) do
-		elem = :array.get(0, array)
-		function.(elem, result)
-	end
+  defp _foldr(array, function, result, 0) do
+    elem = :array.get(0, array)
+    function.(elem, result)
+  end
 
-	defp _foldr(array, function, result, idx) do
-		elem = :array.get(idx, array)
-		result = function.(elem, result)
-		_foldr(array, function, result, idx - 1)
-	end
+  defp _foldr(array, function, result, idx) do
+    elem = :array.get(idx, array)
+    result = function.(elem, result)
+    _foldr(array, function, result, idx - 1)
+  end
 
-	def fill(size, function) do
-		array = :array.new(size)
-		_fill(array, function, size - 1)
-	end
+  def fill(size, function) do
+    array = :array.new(size)
+    _fill(array, function, size - 1)
+  end
 
-	defp _fill(array, function, 0) do 
-		value = function.()
-		:array.set(0, value, array)
-	end
+  defp _fill(array, function, 0) do 
+    value = function.()
+    :array.set(0, value, array)
+  end
 
-	defp _fill(array, function, idx) do
-		value = function.()
-		array = :array.set(idx, value, array)
-		_fill(array, function, idx - 1)
-	end
+  defp _fill(array, function, idx) do
+    value = function.()
+    array = :array.set(idx, value, array)
+    _fill(array, function, idx - 1)
+  end
 
-	def map(in_array, function) do
-		size = :array.size(in_array)
-		out_array = :array.new(size)
-		_map(in_array, function, out_array, size - 1)
-	end
+  def map(in_array, function) do
+    size = :array.size(in_array)
+    out_array = :array.new(size)
+    _map(in_array, function, out_array, size - 1)
+  end
 
-	defp _map(in_array, function, out_array, 0) do
-		in_value = :array.get(0, in_array)
-		out_value = function.(in_value)
-		:array.set(0, out_value, out_array)
-	end
+  defp _map(in_array, function, out_array, 0) do
+    in_value = :array.get(0, in_array)
+    out_value = function.(in_value)
+    :array.set(0, out_value, out_array)
+  end
 
-	defp _map(in_array, function, out_array, idx) do
-		in_value = :array.get(0, in_array)
-		out_value = function.(in_value)
-		out_array.set(0, out_value, out_array)
-		_map(in_array, function, out_array, idx - 1)
-	end
+  defp _map(in_array, function, out_array, idx) do
+    in_value = :array.get(0, in_array)
+    out_value = function.(in_value)
+    out_array.set(0, out_value, out_array)
+    _map(in_array, function, out_array, idx - 1)
+  end
 
-	def random_bit() do
-		:random.uniform(2) - 1
-	end
+  def random_bit() do
+    :random.uniform(2) - 1
+  end
 
-	def random_idx(array) do
-		:random.uniform(:array.size(array)) - 1
-	end		
+  def random_idx(array) do
+    :random.uniform(:array.size(array)) - 1
+  end   
 
-	def random_elem(array) do
-		idx = random_idx(array)
-		:array.get(idx, array)
-	end
+  def random_elem(array) do
+    idx = random_idx(array)
+    :array.get(idx, array)
+  end
 
-	def flip(value) do
-		rem(value + 1, 2)
-	end
+  def flip(value) do
+    rem(value + 1, 2)
+  end
 end
--- test/maxsatproblem_test.exs
+++ test/maxsatproblem_test.exs
@@ -4,11 +4,11 @@ defmodule MaxSATProblemTest do
   use ExUnit.Case
 
   test "read problem" do
-  	problem = MaxSATFunctions.read_problem("data/problems/uf250-01.cnf")
-  	assert problem.num_variables == 250
-  	assert problem.num_clauses == 1065
-  	assert length(problem.clauses) == problem.num_clauses
-  	assert Enum.all?(problem.clauses, fn c -> length(c) == 3 end)
+    problem = MaxSATFunctions.read_problem("data/problems/uf250-01.cnf")
+    assert problem.num_variables == 250
+    assert problem.num_clauses == 1065
+    assert length(problem.clauses) == problem.num_clauses
+    assert Enum.all?(problem.clauses, fn c -> length(c) == 3 end)
   end
 end
 
